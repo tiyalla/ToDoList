a243d49a319334f0195b42cc04db6b0eb4086a7b
Tonye Iyalla <tiyalla2015@gmail.com>
2018-10-01 00:28:38 -0400

    Add unit test for ToDo class

--- ToDoListApp/src/app/todo.spec.ts
+++ ToDoListApp/src/app/todo.spec.ts
@@ -4,4 +4,10 @@ describe('Todo', () => {
   it('should create an instance', () => {
     expect(new Todo()).toBeTruthy();
   });
+
+  it('should accept values in the constructor', () => {
+    let toDo = new Todo({title: 'hi', complete:true });
+    expect(todo.title).toEqual('hi');
+    expect(todo.complete).toEqual(true);
+  })
 });
